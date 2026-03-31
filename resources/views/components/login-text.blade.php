2493a112ccd0c5468ca3465904af8347ef6e43fa
Bhira Piromsopa <s64146@satitm.chula.ac.th>
2026-03-31 23:03:52 +0700

    Update consent text for VATSIM Thailand vACC

--- resources/views/components/login-text.blade.php
+++ resources/views/components/login-text.blade.php
@@ -1,5 +1,8 @@
 <div>
     <p>
-        Ich bin einverstanden damit, dass ich beim Erstellen von Inhalten in der VATSIM Germany Knowledgebase, VATSIM Germany ein nicht ausschließliches unentgeltliches, zeitlich, örtlich und inhaltlich unbeschränktes, übertragbares, unterlizenzierbares und unwiderrufliches Nutzungsrecht an den von mir erstellten Inhalten einräume. Ich garantiere, dass an den von mir erstellten Inhalten keine Rechte Dritter bestehen und durch die eingeräumten Nutzungsrechte keine Rechte Dritter verletzt werden.
+        You consent to the collection, use, and retention of your personal data by VATSIM Thailand vACC in accordance with the Personal Data Protection Act B.E. 2562 (PDPA) of Thailand.
+    </p>
+    <p>
+        ท่านยินยอมให้ VATSIM Thailand vACC เก็บรวบรวม ใช้ และเก็บรักษาข้อมูลส่วนบุคคลของท่าน ตามพระราชบัญญัติคุ้มครองข้อมูลส่วนบุคคล พ.ศ. 2562 (PDPA) ของประเทศไทย
     </p>
 </div>
